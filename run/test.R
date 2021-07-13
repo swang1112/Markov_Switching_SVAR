74d350689e78865e3303329108c89ef24b19f42e
swang1112 <59112676+swang1112@users.noreply.github.com>
2021-07-13 11:07:19 +0200

    Update test.R
    check speed

--- run/test.R
+++ run/test.R
@@ -47,7 +47,6 @@ B     = C %*% Q
 
 U = make_dat(0, Tob = Tob, dof = dof, B = B)
 
-Rcpp::sourceCpp('../fun/kl_fast.cpp')
 # calculate kernel log likelihood at true theta ---------------------------
 get_kl0(theta, u = U, C = C)
 kl_fast(theta, u = U, C = C)
@@ -64,4 +63,39 @@ bench %>% autoplot
 bench %>% show
 
 
+# check givens ------------------------------------------------------------
+# function to compute the optimal B matrix
+# rotmat <- function(pv, faklow) {
+#   ts_dim <- (1 + sqrt(1 + 8 * length(pv))) / 2
+#   combns <- combn(ts_dim, 2, simplify = FALSE)
+#   rotmat <- diag(ts_dim)
+#   for (i in seq_along(pv)) {
+#     tmp <- diag(ts_dim)
+#     tmp[combns[[i]][1], combns[[i]][1]] <- cos(pv[i])
+#     tmp[combns[[i]][2], combns[[i]][2]] <- cos(pv[i])
+#     tmp[combns[[i]][1], combns[[i]][2]] <- - sin(pv[i])
+#     tmp[combns[[i]][2], combns[[i]][1]] <- sin(pv[i])
+#     rotmat <- rotmat %*% tmp
+#   }
+#   tcrossprod(faklow, rotmat)
+# }
+# 
+# theta = pi/3
+# givensQ_fast(theta, 2)-rotmat(theta, diag(2))
+# 
+# theta = c(.55, .75, .234)
+# givensQ_fast(theta, 3)-rotmat(theta, diag(3))
+# 
+# theta = pi/3
+# givensQ_fast(theta, 2)-rotmat(theta, diag(2))
+# 
+# theta = c(.55, .75, .234,  .7, 1.2, 2.4)
+# givensQ_fast(theta, 4)-rotmat(theta, diag(4))
+# 
+# bench2 = microbenchmark::microbenchmark("R" = rotmat(theta, diag(4)),
+#                                        "Rcpp" = givensQ_fast(theta, 4), 
+#                                        times = 100)
+# require(ggfortify)
+# bench2 %>% autoplot
+# bench2 %>% show
 
