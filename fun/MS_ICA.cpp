182f3cc1c58a3ed7ddc66ecd8f52de5a66164000
swang1112 <59112676+swang1112@users.noreply.github.com>
2021-07-21 15:37:21 +0200

    Update MS_ICA.cpp

--- fun/MS_ICA.cpp
+++ fun/MS_ICA.cpp
@@ -144,6 +144,88 @@ double loglike_MS_ICA( arma::vec& theta,  arma::mat& r,  arma::mat& C, arma::vec
   return llv;
 }
 
+arma::vec tdens_joint(arma::mat &E, int & K, int& Tob, arma::mat & S, arma::mat &Q, arma::vec &lambda)
+{
+  arma::vec out(Tob);
+  arma::vec tdist(K);
+  for (int i = 0; i < Tob; ++i) {
+    arma::vec shock = E.col(i);
+    
+    for (int j = 0; j < K; j++) {
+      tdist(j) = R::dt(shock(j), lambda(j), 0);
+    }
+    out(i) = arma::sum(log(tdist)) + log(arma::det(arma::inv_sympd(S) * Q.t()));
+  }
+  return out;
+}
+
+// Log-Likelihood function
+// [[Rcpp::export]]
+double loglike_ngMS_ICA( arma::vec& theta,  arma::mat & u_st, arma::vec & init) 
+{
+  
+  int K     = u_st.n_cols;
+  int Tob   = u_st.n_rows;
+  
+  // Switching Probabilities
+  double p11 = theta(theta.n_elem - 2);
+  double p12 = 1-p11;
+  double p22 = theta(theta.n_elem - 1);
+  double p21 = 1-p22;
+  
+  // structural parameters
+  arma::vec rot = theta.subvec(0, K*(K-1)-1);
+  arma::vec sig = theta.subvec(K*(K-1), K*(K-1) + 2*K-1);
+  arma::vec lambda = theta.subvec(K*(K-1) + 2*K, theta.n_elem - 3);
+  
+  //check for identification 
+  if(p11<=0 || p11>=1 || p22<=0 || p22>= 1 || any(sig < 0.0)|| any(lambda < 2.0)){
+    return 1e25;
+  }
+  
+  //initial state
+  double p1t = init(0);
+  double p2t = init(1);
+  
+  // rotation matrices
+  arma::mat Q_1 = givensQ_fast(rot.subvec(0, 2), K);
+  arma::mat Q_2 = givensQ_fast(rot.subvec(3, 5), K);
+  
+  // structural covariances
+  arma::mat S_1 = arma::diagmat(1/sig.subvec(0, 2));
+  arma::mat S_2 = arma::diagmat(1/sig.subvec(3, 5));
+  
+  // dof
+  arma::vec dof_1 = lambda.subvec(0,2);
+  arma::vec dof_2 = lambda.subvec(3,5);
+  
+  // transform series (B = CQ)
+  arma::mat E_1 = arma::inv_sympd(S_1) * Q_1.t() * u_st.t();
+  arma::mat E_2 = arma::inv_sympd(S_2) * Q_2.t() * u_st.t();
+  
+  
+  // Joint density
+  arma::vec dens_M1 = tdens_joint(E_1, K, Tob, S_1, Q_1, dof_1);
+  arma::vec dens_M2 = tdens_joint(E_2, K, Tob, S_2, Q_2, dof_2);
+
+  double llv = -log(arma::as_scalar(p1t*exp(dens_M1(0)) + p2t*exp(dens_M2(0)) ));
+  
+  for (int i = 1; i < Tob; i++) {
+    
+    
+    double llv_temp1 = arma::as_scalar((p1t*p11+p2t*p21)*exp(dens_M1(i)));
+    double llv_temp2 = arma::as_scalar((p1t*p12+p2t*p22)*exp(dens_M2(i)));
+    
+    double llv_temp = arma::as_scalar(llv_temp1+llv_temp2);
+    p1t = llv_temp1/llv_temp;
+    p2t = llv_temp2/llv_temp;
+    
+    llv-=log(llv_temp);
+    
+  }
+  return llv;
+}
+
 // Log-Likelihood function
 // [[Rcpp::export]]
 double loglike_MS_ICA_M3( arma::vec& theta,  arma::mat& r,  arma::mat& C, arma::vec & init) 
